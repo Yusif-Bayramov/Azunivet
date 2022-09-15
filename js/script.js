3551e544e363c833e9a20426d74536f329e9ad47
Yusif Bayramov <byusub2@gmail.com>
2022-09-15 23:26:09 +0400

    partner and font-size edit

--- js/script.js
+++ js/script.js
@@ -5,3 +5,28 @@ lg.forEach(element => {
         this.classList.add("active")
     });
 });
+
+$(document).ready(function() {
+    $('.customer-logos').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: true,
+        dots: false,
+        pauseOnHover: true,
+        prevArrow: '<i class="slick-prev fas fa-angle-left"></i>',
+        nextArrow: '<i class="slick-next fas fa-angle-right"></i>',
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 2
+            }
+        }]
+    });
+});
