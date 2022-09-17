0ad734e07ed81c3f75bd893e1a2180b5d1295338
Yusif Bayramov <byusub2@gmail.com>
2022-09-18 00:34:39 +0400

    text color edit and about page edit

--- js/script.js
+++ js/script.js
@@ -5,29 +5,3 @@ lg.forEach(element => {
         this.classList.add("active")
     });
 });
-
-$(document).ready(function() {
-    $('.customer-logos').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1500,
-        arrows: true,
-        dots: false,
-        loop: true,
-        pauseOnHover: true,
-        prevArrow: '<i class="slick-prev fas fa-angle-left"></i>',
-        nextArrow: '<i class="slick-next fas fa-angle-right"></i>',
-        responsive: [{
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 3
-            }
-        }, {
-            breakpoint: 520,
-            settings: {
-                slidesToShow: 2
-            }
-        }]
-    });
-});
